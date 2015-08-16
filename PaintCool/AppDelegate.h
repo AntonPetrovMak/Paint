09bc385384e58f1d80654f9020eba5dd2a463b3c
MaksymP <0372@ukr.net>
2015-08-16 13:15:10 +0300

    Initial Commit

---
+++ PaintCool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaintCool
+//
+//  Created by iMac309 on 16.08.15.
+//  Copyright (c) 2015 iMac309. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PaintCool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PaintCool
+//
+//  Created by iMac309 on 16.08.15.
+//  Copyright (c) 2015 iMac309. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
